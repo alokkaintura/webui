7e16c9496dd462c1e10a2e552cf8515fd1b5ab78
Alok Kaintura <66751016+alokkaintura@users.noreply.github.com>
2020-06-13 22:21:50 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,14 +1,11 @@
- <center> WECOME TO MY CLOUD WORLD </center> 
+  <center> WECOME TO MY CLOUD WORLD </center> 
 <br />
 <pre>
-<?php
-echo' 
+ 
 <html>
 <center> <img src="https://github.com/alokkaintura/redhat.git"  width="300" height="400">  </center>
 </html>
-';
-echo'
-<center> <h2> hey cgdfjrdjt </h2> </center>
-';
-?>
+
+<center> <h2> Great initiative by LinuxWorld Informatics Pvt. Ltd. Making India Future Ready </h2> </center>
 </pre>
+
