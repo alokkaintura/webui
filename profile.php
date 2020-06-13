99b60c83496f6c5eeb9dc35221a5c0f294c3079c
Alok Kaintura <66751016+alokkaintura@users.noreply.github.com>
2020-06-13 22:48:51 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,8 +1,9 @@
-  <center> Task 1 - Hybrid Multi Cloud Computing </center> 
+ <center> Task 1 Hybrid Multi Cloud computing Completed </center> 
 <br />
 <pre>
- <?php
-echo '<center> <h2> Terraform is an open-source infrastructure as code software tool created by HashiCorp. It enables users to define and provision a datacenter infrastructure using a high-level configuration language known as Hashicorp Configuration Language, or optionally JSON.  Ready </h2> </center>';
+<?php
+echo'
+<center> <h1> Terraform is an open-source infrastructure as code software tool created by HashiCorp </h1> </center>
+';
 ?>
 </pre>
-
