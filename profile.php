1c51a147fe13ca91413406c3f922e4450459ee84
alokkaintura <66751016+alokkaintura@users.noreply.github.com>
2020-06-13 16:02:26 +0530

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,15 @@
+echo ' <marquee>Multi Hybrid Cloud Computing </marquee> ';
+<br />
+<pre>
+<?php
+echo' 
+<html>
+<center> <img src="https://github.com/Himjo/terra-image.git"  width="300" height="400">  </center>
+</html>
+';
+echo'
+<center> <h2> Terraform </h2> </center>
+     <p> Use Infrastructure as Code to provision and manage any cloud, infrastructure or service </p>
+';
+?>
+</pre>
