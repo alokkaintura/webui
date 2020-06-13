328163156f205387408f13dcfaabb5ac820ff3a8
alokkaintura <66751016+alokkaintura@users.noreply.github.com>
2020-06-13 18:18:57 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,15 +1,11 @@
-<marquee>Multi Hybrid Cloud Computing </marquee>
+<marquee> WECOME TO HYBRID MULTI CLOUD WORLD - BY MR. VIMAL DAGA </marquee> 
 <br />
 <pre>
-<?php
-echo' 
+ 
 <html>
-<center> <img src="https://github.com/Himjo/terra-image.git"  width="300" height="400">  </center>
+<center> <img src="https://raw.githubusercontent.com/Himjo/bonzovi/master/vimal-sir.jpg"  width="300" height="400">  </center>
+
 </html>
-';
-echo'
-<center> <h2> Terraform </h2> </center>
-     <p> Use Infrastructure as Code to provision and manage any cloud, infrastructure or service </p>
-';
-?>
+
+<center> <h2> Terraform - Use Infrastructure as Code to provision and manage any cloud, infrastructure or service </h2> </center>
 </pre>
